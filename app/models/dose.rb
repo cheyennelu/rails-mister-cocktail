0f7ddff5048a4df09de262c7a17aeb77c6312d2a
Cheyenne Lu <cheyenne.lu@gmail.com>
2021-02-19 12:28:33 +0800

    controller for cocktail up

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   validates :description, presence: true
-  validates_uniqueness_of :cocktail, scope: :ingredient
+  validates_uniqueness_of :ingredient, scope: :cocktail
   belongs_to :cocktail
   belongs_to :ingredient
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+raw_json = open(url).read
+json = JSON.parse(raw_json)
+
+ing_array = json['drinks']
+
+puts "let's get ingredients..."
+
+Ingredient.destroy_all
+
+ing_array.each do |hash|
+  ingredient = hash['strIngredient1']
+  Ingredient.create(name: ingredient)
+end
+
+
+puts "all done!"
