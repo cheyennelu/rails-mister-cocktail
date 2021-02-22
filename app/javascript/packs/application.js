93b892bf4d08c3c50129fdbcf65c725d7fbd064f
Cheyenne Lu <cheyenne.lu@gmail.com>
2021-02-22 11:02:32 +0800

    remember not to use = in iteration part of erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,10 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
